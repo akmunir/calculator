ac566b4315259f6740d19bc675d1234fbb51d67d
Asim <akm02145@gmail.com>
2024-05-27 10:00:56 -0400

    refactored html and css, all of the buttons now have the same class instead of seperate ones

---
+++ calculatorScript.js
@@ -0,0 +1,9 @@
+let currentOutput = document.querySelector(".output");
+ document
+.querySelector(".calc-buttons")
+.addEventListener("click", function(event)
+{
+    console.log("1");
+    let input = event.target.
+    console.log("test" + input);
+});
