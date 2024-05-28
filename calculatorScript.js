0c1ca88047a0f7df921e08373ca79838e0428723
Asim <akm02145@gmail.com>
2024-05-27 22:45:21 -0400

    Issues with logic when performing an operation using the previous answer, needs fix

--- calculatorScript.js
+++ calculatorScript.js
@@ -5,27 +5,25 @@
     let currentOperator = "";
     let ans = 0;
     let flag = false;
+    
 
 document
 .querySelector(".calc-container")
 .addEventListener("click", function(event)
 {   if (event.target.classList.contains("operator") 
         || event.target.classList.contains("compute") 
-        || event.target.classList.contains("clear")
         || event.target.classList.contains("del"))
     {
-        console.log("cleano aa");
-        if (currentInput1 === 0 && currentInput2 == 0)
-            {
-                 return;
-            }
+        console.log("aa");
+        if (currentInput1 === 0 && currentInput2 === 0)
+            return;
            
     }
     if (event.target.classList.contains("operator"))
     {
         if (currentInput1 === 0) 
             currentInput1 = ans;  
-        currentOutput.innerText = 0;
+        currentOutput.innerText = " ";
         flag = true;
         currentOperator = event.target.innerText;
     }
@@ -43,12 +41,17 @@ document
             currentInput2 = 0;
         }
         else 
-            ans = currentInput1 / currentInput2;
+        {
+            if (currentInput2 === 0) {
+                currentOutput.innerText = "nuh uh";
+                return;
+            } else              
+                ans = currentInput1 / currentInput2;
+        }
         currentOutput.innerText = ans;   
     }
     else if (event.target.classList.contains("clear"))
         {
-            console.log("cleared");
             ans = 0; 
             currentInput1 = 0;
             currentInput2 = 0;
@@ -57,9 +60,11 @@ document
         }
     else if (flag)
     {
+        console.log("passed")
         currentInput2 *= 10;
         currentInput2 += Number(event.target.innerText);
         currentOutput.innerText = currentInput2;
+        console.log(currentInput2);
     } else if(event.target.classList.contains("del"))
     {
         if (flag) {
@@ -70,9 +75,6 @@ document
             currentInput1 = Math.floor(currentInput1 / 10);
             currentOutput.innerText = currentInput1;
         }
-
-            
-        
     }
     else 
     {
