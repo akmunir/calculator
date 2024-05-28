5988071285640b29cf6dd0503b696c177ea0b323
Asim <akm02145@gmail.com>
2024-05-27 20:25:24 -0400

    Calculation logic implemented, need to add clear, and backspace

--- calculatorScript.js
+++ calculatorScript.js
@@ -1,9 +1,52 @@
-let currentOutput = document.querySelector(".output");
- document
-.querySelector(".calc-buttons")
+
+    let currentOutput = document.querySelector(".output");
+    let currentInput1 = 0;
+    let currentInput2 = 0;
+    let currentOperator = "";
+    let ans = 0;
+    let flag = false;
+
+document
+.querySelector(".calc-container")
 .addEventListener("click", function(event)
 {
-    console.log("1");
-    let input = event.target.
-    console.log("test" + input);
+    if (event.target.classList.contains("operator"))
+    {
+        if (currentInput1 === 0) 
+            currentInput1 = ans;  
+        currentOutput.innerText = 0;
+        flag = true;
+        currentOperator = event.target.innerText;
+    }
+    else if (event.target.classList.contains("compute"))
+    {
+        if (currentOperator === "+") 
+            ans = currentInput1 + currentInput2;
+        else if (currentOperator === "-")
+            ans = currentInput1 - currentInput2;
+        else if (currentOperator === "x") {
+            ans = currentInput1 * currentInput2;
+            flag = false;
+            currentOperator = "";
+            currentInput1 = 0;
+            currentInput2 = 0;
+        }
+        else
+            ans = currentInput1 / currentInput2;
+        currentOutput.innerText = ans;
+
+        
+    }
+    else if (flag)
+    {
+        currentInput2 *= 10;
+        currentInput2 += Number(event.target.innerText);
+        currentOutput.innerText = currentInput2;
+    }
+    else 
+    {
+        currentInput1 *= 10;
+        currentInput1 += Number(event.target.innerText);
+        currentOutput.innerText = currentInput1;
+    } 
 });
