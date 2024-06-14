b5d5f41246e535e48d1b40cee7e19e443c5dcc02
Asim <akm02145@gmail.com>
2024-06-13 23:31:29 -0400

    Extracted the logic from the conditional checks, and put them into functions

--- calculatorScript.js
+++ calculatorScript.js
@@ -4,82 +4,104 @@
     let currentInput2 = 0;
     let currentOperator = "";
     let ans = 0;
-    let flag = false;
+    let isNewInput = false;
     
 
 document
 .querySelector(".calc-container")
 .addEventListener("click", function(event)
-{   if (event.target.classList.contains("operator") 
-        || event.target.classList.contains("compute") 
-        || event.target.classList.contains("del"))
+{  
+    const target = event.target;
+   
+    if (target.classList.contains("operator") 
+        || target.classList.contains("compute") 
+        || target.classList.contains("del"))
     {
-        console.log("aa");
         if (currentInput1 === 0 && currentInput2 === 0)
             return;
            
     }
-    if (event.target.classList.contains("operator"))
+    if (target.classList.contains("operator"))
     {
-        if (currentInput1 === 0) 
-            currentInput1 = ans;  
-        currentOutput.innerText = " ";
-        flag = true;
-        currentOperator = event.target.innerText;
+        checkOperator(target.innerText);
     }
-    else if (event.target.classList.contains("compute"))
+    else if (target.classList.contains("compute"))
     {
-        if (currentOperator === "+") 
-            ans = currentInput1 + currentInput2;
-        else if (currentOperator === "-")
-            ans = currentInput1 - currentInput2;
-        else if (currentOperator === "x") {
-            ans = currentInput1 * currentInput2;
-            flag = false;
-            currentOperator = "";
-            currentInput1 = 0;
-            currentInput2 = 0;
-        }
-        else 
-        {
-            if (currentInput2 === 0) {
-                currentOutput.innerText = "nuh uh";
-                return;
-            } else              
-                ans = currentInput1 / currentInput2;
-        }
-        currentOutput.innerText = ans;   
+        compute();
     }
-    else if (event.target.classList.contains("clear"))
-        {
-            ans = 0; 
-            currentInput1 = 0;
-            currentInput2 = 0;
-            currentOperator = "";
-            currentOutput.innerText = ans;
-        }
-    else if (flag)
+    else if (target.classList.contains("clear"))
     {
-        console.log("passed")
-        currentInput2 *= 10;
-        currentInput2 += Number(event.target.innerText);
-        currentOutput.innerText = currentInput2;
-        console.log(currentInput2);
-    } else if(event.target.classList.contains("del"))
+           clearCalculator();
+        
+    } else if(target.classList.contains("del"))
     {
-        if (flag) {
-            currentInput2 = Math.floor(currentInput2 / 10);
-            currentOutput.innerText = currentInput2;
-        }  
-        else {
-            currentInput1 = Math.floor(currentInput1 / 10);
-            currentOutput.innerText = currentInput1;
-        }
+        deletion();
+    } else if (isDigit(target.innerText)) {
+        handleDigit(target.innerText);
+    }
+    
+});
+
+function checkOperator(operator) {
+    if (currentInput1 === 0 && ans !== 0) 
+        currentInput1 = ans;  
+    currentOutput.innerText = " ";
+    isNewInput = true;
+    currentOperator = event.target.innerText;
+}
+
+function compute() {
+    if (currentOperator === "+") 
+        ans = currentInput1 + currentInput2;
+    else if (currentOperator === "-")
+        ans = currentInput1 - currentInput2;
+    else if (currentOperator === "x") {
+        ans = currentInput1 * currentInput2;
+        isNewInput = false;
+        currentOperator = "";
+        currentInput1 = 0;
+        currentInput2 = 0;
     }
     else 
     {
-        currentInput1 *= 10;
-        currentInput1 += Number(event.target.innerText);
+        if (currentInput2 === 0) {
+            currentOutput.innerText = "invalid";
+            return;
+        } else              
+            ans = currentInput1 / currentInput2;
+    }
+    currentOutput.innerText = ans;   
+}
+
+function clearCalculator() {
+    ans = 0; 
+    currentInput1 = 0;
+    currentInput2 = 0;
+    currentOperator = "";
+    currentOutput.innerText = ans;
+}
+
+function deletion() {
+    if (isNewInput) {
+        currentInput2 = Math.floor(currentInput2 / 10);
+        currentOutput.innerText = currentInput2;
+    }  
+    else {
+        currentInput1 = Math.floor(currentInput1 / 10);
         currentOutput.innerText = currentInput1;
-    } 
-});
+    }
+}
+
+function handleDigit(target) {
+    if (isNewInput) {
+        currentInput2 = (currentInput2 * 10) + Number(target);
+        currentOutput.innerText = currentInput2;
+    } else {
+        currentInput1 = (currentInput1 * 10) + Number(target);
+        currentOutput.innerText = currentInput1;
+    }
+}
+
+function isDigit(target) {
+    return /^\d$/.test(target);
+}
